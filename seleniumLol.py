56497d2e4da0e38d086bf02271edb432a7981702
Thales Vieira <thales.dartech@gmail.com>
2022-03-23 10:42:46 -0300

    seleniumLoL.py
    Puxei os nomes dos patchs notes via selenium

--- seleniumLol.py
+++ seleniumLol.py
@@ -1,28 +1,33 @@
-# Importando o Selenium 
+# IMPORTANDO O SELENIUM
+from socket import timeout
 from  selenium import webdriver
 import time
 
+# ACESSANDO A PÁGINA
 driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
 driver.get('https://www.leagueoflegends.com/pt-br/news/tags/patch-notes/')
 driver.maximize_window()
 
-# Clicar em Carregar mais
+# CLICANDO EM CARREGAR MAIS
 driver.execute_script('document.getElementsByClassName("style__LoadMoreButton-sc-1ynvx8h-5")[0].click()')
 
-# Buscando informações da página
+# TIMEOUT PARA CARREGAR OS OUTROS 6 PATCH NOTES
+time.sleep(2)
+
+# BUSCANDO AS INFORMAÇÕES DA PÁGINA
 h2 = driver.find_elements_by_tag_name('h2')
 
-# Visualizando os títulos 
+# PEGANDO O NOME DOS PATCH NOTES
 for c in range(0,len(h2)):
     print(h2[c].text)
 
-#Timeout na Página
-time.sleep(10)
+# FECHANDO A PÁGINA
+driver.quit()
+    
 
 
-
-
-
-
-
-
+    #driver.execute_script("window.open('');") #abre uma aba nova
+    #driver.switch_to.window(driver.window_handles[c]) #seleciona a aba que foi aberta
+    #driver.get('https://www.leagueoflegends.com/pt-br/news/tags/patch-notes/') #Entra na URL de patch notes.
+    #Clica em carregar mais
+    #driver.execute_script('document.getElementsByClassName("style__Title-sc-1h41bzo-8 fEywOQ")[0].click()') # Clica em algum dos patch notes.
